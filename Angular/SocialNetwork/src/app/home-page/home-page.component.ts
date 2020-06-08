27bec478843219defa969a09df4fea7d4be2b067
Andres Aparicio <andresmijin@gmail.com>
2020-06-08 17:59:00 -0400

    toggle buttons

--- Angular/SocialNetwork/src/app/home-page/home-page.component.ts
+++ Angular/SocialNetwork/src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'home-page',
@@ -6,7 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
+  public show:boolean = false;
+  public buttonName:any = 'Show';
+  public show2:boolean = false;
+  public buttonName2:any = 'Show2';
+  public show3:boolean = false;
+  public buttonName3:any = 'Show3';
   constructor() {}
 
   ngOnInit(): void {}
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show)  
+      this.buttonName = "My profile button";
+    else
+      this.buttonName = "My profile button";
+  }
+  toggle2() {
+    this.show2 = !this.show2;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show2)  
+      this.buttonName2 = "Modify Info Button";
+    else
+      this.buttonName2 = "Modify Info Button";
+  }
+
+  toggle3() {
+    this.show3 = !this.show3;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.show3)  
+      this.buttonName3 = "Search network Button";
+    else
+      this.buttonName3 = "Search network Button";
+  }
 }
