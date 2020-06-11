791e738b148f6d2e4e6b73ae3e89e7b80fcc0abd
Andres Aparicio <andresmijin@gmail.com>
2020-06-11 00:13:19 -0400

    Andres Angular code

--- Angular/SocialNetwork/src/app/profile-information/profile-information.component.ts
+++ Angular/SocialNetwork/src/app/profile-information/profile-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'profile-information',
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-information.component.css'],
 })
 export class ProfileInformationComponent implements OnInit {
+  @Input('type') type;
+  placeholderText;
+
+  user = {
+    fullName: 'Harry Potter',
+    picture:
+      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    email: 'harrypotter@revature.com',
+    bio: 'I like magic',
+  };
+  picture;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.placeholderText = `Please enter your ${this.type}`;
+    this.picture = {
+      'background-image': 'url(' + this.user.picture + ')',
+    };
+  }
 }
--- Angular/SocialNetwork/src/app/update-profile/update-profile.component.ts
+++ Angular/SocialNetwork/src/app/update-profile/update-profile.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-profile.component.css'],
 })
 export class UpdateProfileComponent implements OnInit {
+ // @Input('type') type;
+  placeholderText;
+
+  user = {
+    fullName: '',
+    picture:
+      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    email:'',
+    bio:'',
+  };
+  picture;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+   // this.placeholderText = `Please enter your ${this.type}`;
+    this.picture = {
+      'background-image': 'url(' + this.user.picture + ')',
+    };
+  }
 }
